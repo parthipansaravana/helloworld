56387ecf3c98020e84e5bca2f930aa60f66de390
parthipansaravanan <80159011+parthipansaravanan@users.noreply.github.com>
2022-12-19 18:48:34 +0530

    added string to float arithmetic operation

--- helloworld/input function.py
+++ helloworld/input function.py
@@ -10,3 +10,9 @@ name=input("what is your name\n")
 age=input("what is your age\n")
 location=input("where are you from\n")
 print("my name is "+name+" i am "+age+" years old and i am from "+location)
+
+# get input as flote and do arithmetic operation
+X=input("enter x value")
+Y=input("enter y value ")
+Z=float(X)*float(Y)
+print("X*Y="+str(Z))

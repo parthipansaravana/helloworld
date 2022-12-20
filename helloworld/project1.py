a53958e1bcf4c5475c93665da55f93b920f740cc
parthipansaravanan <80159011+parthipansaravanan@users.noreply.github.com>
2022-12-20 12:00:18 +0530

    simple application to calculate bill with the user input

--- helloworld/project1.py
+++ helloworld/project1.py
@@ -9,4 +9,4 @@ dhal_price=input("\n enter the price of the dhal:")
 bag_price=input("\n enter the price of the bag:")
 
 bill=(float(rice)*float(rice_price))+(float(dhal)*float(dhal_price))+(float(bag)*float(bag_price))
-print("\ntotal bill ="+str(bill))
+print("\ntotal bill ="+str(bill)) 

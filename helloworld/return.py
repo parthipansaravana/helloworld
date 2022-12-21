0b2dadc5e25e7aa60221d9760c8078fa3fa59ecd
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-21 13:21:59 +0530

    return statement and functionality

---
+++ helloworld/return.py
@@ -0,0 +1,14 @@
+# return return the value and exit from the execution
+# return should use inside the function
+def hello(name):
+    print("hello "+name)
+    return
+    print("bye")
+
+def bye(name):
+    print("hello "+name)
+    print("bye "+name)
+    return
+
+hello("parthi")
+bye("parthi")

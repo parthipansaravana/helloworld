95e3209b7b42ea0db73df302cc6cf87778ccbad7
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-21 12:39:47 +0530

    utility function and function call in python

---
+++ helloworld/utility function.py
@@ -0,0 +1,15 @@
+# FUNCTION DEFINATION
+def love(name):
+    print("i love "+name)
+
+def christmas(name):
+    print("marry christmas "+name)
+
+# INPUT FUNCTION
+A=input("enter the name of the person you love: ")
+
+# FUNCTION CALL
+love(A)
+christmas(A)
+
+

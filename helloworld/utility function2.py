8746c367c375ebfdba407223709cc87b44e02f20
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-21 12:40:33 +0530

    simple operation in hotel reception using utility function

---
+++ helloworld/utility function2.py
@@ -0,0 +1,11 @@
+def rec(name):
+    print(name+",how can i help you.")
+def res(name,food,table,beverage):
+    name=input("may i know your name:")
+    food=input("What did you like to eat:")
+    table=input("May i know your table number:")
+    beverage=input("Did u need any beverage after food:")
+    print(str(name)+" you ordered "+str(food)+ " beverage is " +str(beverage)+" any your table is "+str(table))
+
+rec("Reception")
+res("name","food","table","beveraage")

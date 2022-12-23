a69db0ce525dc54b2a5ae188a6546cca293e96aa
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-23 14:42:11 +0530

    if condition with boolean

---
+++ helloworld/if with boolean.py
@@ -0,0 +1,11 @@
+do_you_like_to_eat=True
+do_you_like_to_drink=False
+
+
+# run if boolean value is true
+if do_you_like_to_eat:
+    print("what did you like to eat.")
+
+# doesnot exwcute if the value is false
+if do_you_like_to_drink:
+    print("what did you like to drink.")

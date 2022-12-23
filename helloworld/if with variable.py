91d6439e1d25fa3cd435b8b37ebe948e10033380
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-23 15:27:26 +0530

    if with variable number variable: in number variable condition statements can be check with thw values. string variables: in string variables we can not check with all condition but we can use the function like string size to check weather the strings are in equal size ect if we compare both the string directly then he charector should be same even its should be in same case.

---
+++ helloworld/if with variable.py
@@ -0,0 +1,15 @@
+# if condition with number variable
+a=5
+b=10
+if a==b:
+    print("a is equal to b:")
+else:
+    print("a and b are not equal")
+
+# if condition with string variable
+c="hello"
+d="Hello"
+if c==d:
+    print("c and d are equal.")
+else:
+    print("c and d are not equal")

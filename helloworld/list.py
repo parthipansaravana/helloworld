0e5367887f6e6fc371d89e1259a62afbeb8f362c
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-23 19:42:16 +0530

    list used to store multiple element in single variable string should be within "" nnumber can pass directly enclosed with []

---
+++ helloworld/list.py
@@ -0,0 +1,17 @@
+#variable
+a="apple"
+
+
+#list
+b=["apple","banana","cherry"]
+
+# print variable
+print(a)
+# print list
+print(b)
+
+# to ptint n-1 value
+print(b[:2])
+
+# to print from to end
+print(b[2:])

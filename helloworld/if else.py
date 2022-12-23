a80ff95b74994e0d150d2324f5d9fc41ce5e76eb
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-23 14:56:17 +0530

    if else condition with boolean

---
+++ helloworld/if else.py
@@ -0,0 +1,17 @@
+do_you_like_to_eat=True
+do_you_like_to_drink=False
+
+
+# run if boolean value is true
+if do_you_like_to_eat:
+    print("what did you like to eat.")
+# if do_you_like_to_eat is false else part will execute
+else:
+    print("we have drinks also")
+
+# doesnot exwcute if the value is false
+if do_you_like_to_drink:
+    print("we have softdrink also")
+# if do_you_like_to_drink is true else will execute
+else:
+    print("what did you like to drink.")

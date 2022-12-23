ba163b44eeb5a4451ed07783567b94066cd79ecf
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-23 15:08:33 +0530

    elif 1st check the if condition if its true then execute the statement and exit.if 1st is false then check with 2nd condition if 2nd condition is true it execute the statement and exit if both the condition is false then execute else statement.if both are true 1st statement will execute.

---
+++ helloworld/else if with boolean.py
@@ -0,0 +1,12 @@
+do_you_like_to_eat=True
+do_you_like_to_drink=True
+
+
+# run if boolean value is true
+if do_you_like_to_eat:
+    print("what did you like to eat.")
+# if conditoin is true then elif condition will check both the conditio become true then the elif part will execute if both the condition is false then else part will execute
+elif do_you_like_to_drink:
+    print("lets have drink")
+else:
+    print("i am good")

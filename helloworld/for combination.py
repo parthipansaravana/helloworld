65557a24948e3fbee1ab52aa373fa9fc2bc942d8
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-24 12:13:11 +0530

    for combination with 3 list

---
+++ helloworld/for combination.py
@@ -0,0 +1,8 @@
+meal=["rice","pizza","burger"]
+drink=["coffee","tea","carbonated drink"]
+desert=["cake","ice cream","falluda"]
+
+for m in meal:
+    for d in drink:
+        for de in desert:
+            print("i will have "+m+" as a meal "+d+" as a drink "+de+" as a desert.")

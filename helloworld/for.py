8d8d3b163ae851acae206d6f4cfdc0f18cc1a3f4
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-24 11:55:09 +0530

    for loop has reange value

---
+++ helloworld/for.py
@@ -0,0 +1,19 @@
+a=[1,2,3,4,5]
+
+# loop to print all the element in the list
+for x in a:
+    print(x)
+
+# loop which break when the value is 4
+for x in a:
+    print(x)
+    if x==4:
+        break
+
+# loop to print number from 0 to 5
+for number in range(6):
+    print(number)
+
+# loop which print the numbers between 6 to 20
+for number in range(6,20):
+    print(number)

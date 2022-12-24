247c99731f29d941e03a2b70bb81cd8b50c925ba
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-24 11:25:33 +0530

    dictionary is enclosed with in {} has key and value

---
+++ helloworld/dictionary.py
@@ -0,0 +1,9 @@
+# dictionary has a address and value inside the {} but there should be no repetation in the key
+# in get function depport value print when the key value is not found
+dictionary={
+    "p":"parthi",
+    "M":"magesh",
+    "P":"palani",
+    "V":"veera"
+}
+print(dictionary.get("V","value not found"))

d18ce55b7d42e10b0716895e3518700c2793ac7e
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-24 11:15:18 +0530

    tupples 1.use when the values are static 2.its cant be change 3.unable to perform operation like insert delete 4.combine into list and perform operation in data

---
+++ helloworld/tupple.py
@@ -0,0 +1,17 @@
+
+# list
+list=["green","yellow","blue"]
+# tupple
+tupple=("red","black","grey")
+
+
+# print list and tupple
+print(list)
+print(tupple)
+
+# difference between the tupple and list are can't able to modify tupple but list can modify we can change the tupple into list and perform any operation in list
+a=(1,2,3,4)
+b=(4,5,6,7)
+# merge two tupples into one
+c=a+b
+print(c)

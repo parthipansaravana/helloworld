a642cb0ce40303446a8319936f4c84dfda71b52c
parthipansaravanan <80308068+parthipansaravana@users.noreply.github.com>
2022-12-24 12:45:22 +0530

    class and object class can create in another file and import into a project and use the function using object the values are passed

---
+++ helloworld/class and object/class1.py
@@ -0,0 +1,5 @@
+class pizza:
+    def __init__(self,type,price,size):
+        self.type=type
+        self.price=price
+        self.size=size
---
+++ helloworld/class and object/main.py
@@ -0,0 +1,6 @@
+from class1 import pizza
+
+# value passed as a object
+pizzav=pizza("vegetrian",4,"small")
+
+print(pizzav.size)
